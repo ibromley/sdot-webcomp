c4dac8b6457710bfd375d683ab97be2a8ab370a5
Ryker Schwartzenberger <rykerls@uw.edu>
2018-04-19 12:30:42 -0700

    forgot one file

--- src/js/main.js
+++ src/js/main.js
@@ -46,17 +46,5 @@ var onScroll = function() {
   }
 }
 
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
 window.addEventListener("scroll", debounce(onScroll, 100));
 onScroll();
