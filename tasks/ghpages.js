68ae94cf3df711f08af9d44331840c7e44724065
Iain Bromley <iaineubromley@gmail.com>
2018-05-28 16:47:23 -0700

    Fixed Grunt tasks

---
+++ tasks/ghpages.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+  
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.initConfig({
+        'gh-pages': {
+            options: {
+                base: 'build'
+            },
+            src: ['**/*']
+        }
+    });
+
+}
